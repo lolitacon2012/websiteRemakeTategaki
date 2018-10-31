008e63a3332d2166c508dbdbcfd72101591e051a
Dake Liu <dake.liu@shopee.com>
2018-10-31 18:29:40 +0800

    Working tategaki reversed scrolling

---
+++ components/pageLayouts/layout_tategaki.js
@@ -0,0 +1,86 @@
+import Head from 'next/head';
+class Layout extends React.Component{
+  constructor(props){
+    super(props)
+    this.realContainer = React.createRef()
+    this.wrapperContainer = React.createRef()
+    this.state = {
+      contentRightOffset: 0,
+      fakeContentHeight: 0,
+    }
+    this.handleScroll = this.handleScroll.bind(this);
+    this.handleScrollViewportChange = this.handleScrollViewportChange.bind(this);
+  }
+  componentDidMount () {
+    this.handleScrollViewportChange();
+    window.addEventListener('resize', this.handleScrollViewportChange);
+  }
+  handleScroll (){
+    const offSetY = this.wrapperContainer.current.scrollTop;
+    this.setState({
+      contentRightOffset: 0 - offSetY
+    })
+    this.handleScrollViewportChange()
+  }
+  handleScrollViewportChange (){
+    const contentWidth = this.realContainer.current.offsetWidth;
+    const contentOverflowWidth = contentWidth - document.documentElement.clientWidth;
+    const scrollableWidth = contentOverflowWidth > 0 ? contentOverflowWidth : 0;
+    const fakeContentHeight = document.documentElement.clientHeight + scrollableWidth;
+    this.setState({
+      fakeContentHeight
+    })
+  }
+  render () {
+    return (
+      <div>
+        <Head>
+          <title>Kannagi Peekumii</title>
+          <meta name="viewport" content="width=device-width, height=device-height initial-scale=1.0, shrink-to-fit=no"></meta>
+          <meta charSet="utf-8" />
+        </Head>
+
+        <div className="real-container" ref={this.realContainer} style={{right: this.state.contentRightOffset}}>
+          {this.props.children}
+        </div>
+        <div ref={this.wrapperContainer} className="wrapper-container" onScroll={this.handleScroll} >
+        <div style={{height: this.state.fakeContentHeight}}>
+        </div>
+        </div>
+        <style jsx global>{`
+        html, body {
+    height: 100%;
+    overflow: hidden;
+    margin: 0;
+    width: 100%;
+}
+        `}
+      </style>
+      <style jsx>{`
+        .real-container {
+          position: fixed;
+          display: flex;
+          flex-direction: row;
+          height: 100vh;
+          overflow-y: hidden;
+          top: 0;
+          bottom: 0;
+        }
+        .wrapper-container {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    overflow: scroll;
+}
+        `}
+      </style>
+    
+    
+      </div>
+    );
+  }
+}
+
+export default Layout;
