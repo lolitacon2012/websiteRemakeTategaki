c6bdd0f1b232b30ae0785f77d6800c424a9b9af1
root <lolitacon2012@gmail.com>
2018-11-25 23:51:39 +0800

    add firework

--- components/pageLayouts/layout_tategaki.js
+++ components/pageLayouts/layout_tategaki.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import { isMobile } from "react-device-detect";
 import font from "../../build/fonts/SourceHanSerifCN-Light.woff";
 import { colors } from "../colors";
+import { debounce } from "../../utils/debounce";
+import anime from "animejs";
 class Layout extends React.Component {
     constructor(props) {
         super(props);
@@ -16,12 +18,14 @@ class Layout extends React.Component {
             this
         );
         this.drawBackgroundCanvas = this.drawBackgroundCanvas.bind(this);
+        this.handleForegroundCanvas = this.handleForegroundCanvas.bind(this);
     }
     componentDidMount() {
         this.handleScrollViewportChange();
         window.addEventListener("resize", this.handleScrollViewportChange);
         window.addEventListener("scroll", this.handleScroll);
         this.drawBackgroundCanvas();
+        this.handleForegroundCanvas();
     }
     handleScroll() {
         const doc = document.documentElement;
@@ -43,7 +47,152 @@ class Layout extends React.Component {
             fakeContentHeight
         });
     }
-    drawOneFrameOfBackgroundCanvas() {}
+    handleForegroundCanvas() {
+        var canvasEl = document.getElementById("foregroundCanvas");
+        if (canvasEl) {
+            var ctx = canvasEl.getContext("2d");
+            var numberOfParticules = 30;
+            var pointerX = 0;
+            var pointerY = 0;
+            // var tap = ('ontouchstart' in window || navigator.msMaxTouchPoints) ? 'touchstart' : 'mousedown'
+            // Fixed the mobile scroll
+            var tap = "mousedown";
+            var colors = ["#FF1461", "#18FF92", "#5A87FF", "#FBF38C"];
+
+            var setCanvasSize = debounce(() => {
+                canvasEl.width = window.innerWidth * 2;
+                canvasEl.height = window.innerHeight * 2;
+                canvasEl.style.width = window.innerWidth + "px";
+                canvasEl.style.height = window.innerHeight + "px";
+                canvasEl.getContext("2d").scale(2, 2);
+            }, 500);
+
+            var render = anime({
+                duration: Infinity,
+                update: function() {
+                    ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
+                }
+            });
+
+            document.addEventListener(
+                tap,
+                function(e) {
+                    if (
+                        e.target.id !== "sidebar" &&
+                        e.target.id !== "toggle-sidebar" &&
+                        e.target.nodeName !== "A" &&
+                        e.target.nodeName !== "IMG"
+                    ) {
+                        render.play();
+                        updateCoords(e);
+                        animateParticules(pointerX, pointerY);
+                    }
+                },
+                false
+            );
+
+            setCanvasSize();
+            window.addEventListener("resize", setCanvasSize, false);
+        }
+
+        function updateCoords(e) {
+            pointerX =
+                (e.clientX || e.touches[0].clientX) -
+                canvasEl.getBoundingClientRect().left;
+            pointerY =
+                e.clientY ||
+                e.touches[0].clientY - canvasEl.getBoundingClientRect().top;
+        }
+
+        function setParticuleDirection(p) {
+            var angle = (anime.random(0, 360) * Math.PI) / 180;
+            var value = anime.random(50, 180);
+            var radius = [-1, 1][anime.random(0, 1)] * value;
+            return {
+                x: p.x + radius * Math.cos(angle),
+                y: p.y + radius * Math.sin(angle)
+            };
+        }
+
+        function createParticule(x, y) {
+            var p = {};
+            p.x = x;
+            p.y = y;
+            p.color = colors[anime.random(0, colors.length - 1)];
+            p.radius = anime.random(16, 32);
+            p.endPos = setParticuleDirection(p);
+            p.draw = function() {
+                ctx.beginPath();
+                ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
+                ctx.fillStyle = p.color;
+                ctx.fill();
+            };
+            return p;
+        }
+
+        function createCircle(x, y) {
+            var p = {};
+            p.x = x;
+            p.y = y;
+            p.color = "#F00";
+            p.radius = 0.1;
+            p.alpha = 0.5;
+            p.lineWidth = 6;
+            p.draw = function() {
+                ctx.globalAlpha = p.alpha;
+                ctx.beginPath();
+                ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
+                ctx.lineWidth = p.lineWidth;
+                ctx.strokeStyle = p.color;
+                ctx.stroke();
+                ctx.globalAlpha = 1;
+            };
+            return p;
+        }
+
+        function renderParticule(anim) {
+            for (var i = 0; i < anim.animatables.length; i++) {
+                anim.animatables[i].target.draw();
+            }
+        }
+
+        function animateParticules(x, y) {
+            var circle = createCircle(x, y);
+            var particules = [];
+            for (var i = 0; i < numberOfParticules; i++) {
+                particules.push(createParticule(x, y));
+            }
+            anime
+                .timeline()
+                .add({
+                    targets: particules,
+                    x: function(p) {
+                        return p.endPos.x;
+                    },
+                    y: function(p) {
+                        return p.endPos.y;
+                    },
+                    radius: 0.1,
+                    duration: anime.random(1200, 1800),
+                    easing: "easeOutExpo",
+                    update: renderParticule
+                })
+                .add({
+                    targets: circle,
+                    radius: anime.random(80, 160),
+                    lineWidth: 0,
+                    alpha: {
+                        value: 0,
+                        easing: "linear",
+                        duration: anime.random(600, 800)
+                    },
+                    duration: anime.random(1200, 1800),
+                    easing: "easeOutExpo",
+                    update: renderParticule,
+                    offset: 0
+                });
+        }
+    }
     drawBackgroundCanvas() {
         const pointSet = [];
         const c = document.getElementById("backgroundCanvas");
@@ -129,6 +278,9 @@ class Layout extends React.Component {
                 </Head>
                 {!isMobile && (
                     <div>
+                        <div className="foreground-canvas-container">
+                            <canvas id="foregroundCanvas" />
+                        </div>
                         <div className="top-purple-pannel" />
                         <div className="bottom-purple-pannel" />
                         <div
@@ -220,7 +372,7 @@ class Layout extends React.Component {
                             width: 100vw;
                             height: 128px;
                             background-color: ${colors.middlePurple};
-                            z-index: 9999;
+                            z-index: 9997;
                         }
                         .bottom-purple-pannel {
                             position: fixed;
@@ -230,6 +382,16 @@ class Layout extends React.Component {
                             width: 120vw;
                             height: 128px;
                             background-color: ${colors.middlePurple};
+                            z-index: 9998;
+                        }
+                        .foreground-canvas-container {
+                            position: fixed;
+                            left: 0;
+                            right: 0;
+                            bottom: 0;
+                            top: 0;
+                            width: 100vw;
+                            height: 100vh;
                             z-index: 9999;
                         }
                     `}
